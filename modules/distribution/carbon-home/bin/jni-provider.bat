3c60e68e2f21f2aad653943222957a8e9afc32a5
gayaldassanayake <g.c.dassanayake@gmail.com>
2026-09-22 09:37:22 +0530

    Remove java17 hard cap

---
+++ modules/distribution/carbon-home/bin/jni-provider.bat
@@ -0,0 +1,44 @@
+@echo off
+rem ---------------------------------------------------------------------------
+rem  Copyright (c) 2019, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+rem
+rem  Licensed under the Apache License, Version 2.0 (the "License");
+rem  you may not use this file except in compliance with the License.
+rem  You may obtain a copy of the License at
+rem
+rem      http://www.apache.org/licenses/LICENSE-2.0
+rem
+rem  Unless required by applicable law or agreed to in writing, software
+rem  distributed under the License is distributed on an "AS IS" BASIS,
+rem  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+rem  See the License for the specific language governing permissions and
+rem  limitations under the License.
+rem ---------------------------------------------------------------------------
+
+setlocal
+
+if "%JAVA_HOME%" == "" goto noJavaHome
+if not exist "%JAVA_HOME%\bin\java.exe" goto noJavaHome
+goto commandLifecycle
+
+:noJavaHome
+echo "You must set the JAVA_HOME variable before running CARBON."
+goto end
+
+:commandLifecycle
+set CMD=RUN %*
+goto runTool
+
+:runTool
+set CURRENT_DIR=%cd%
+
+cd %CARBON_HOME%\bin
+echo JAVA_HOME environment variable is set to %JAVA_HOME%
+echo CARBON_HOME environment variable is set to %CARBON_HOME%
+"%JAVA_HOME%\bin\java" -cp ".\*;..\bin\tools\*" -Dcarbon.home="%CARBON_HOME%" -Dwso2.carbon.tool="jni-provider" org.wso2.carbon.tools.CarbonToolExecutor %1 %2 %3 %4
+
+:end
+goto endlocal
+
+:endlocal
+endlocal
